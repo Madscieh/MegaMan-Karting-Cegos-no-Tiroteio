7acdfddcaa936c5b26eefe708dd638df6f8ada4b
Madscieh <64798875+Madscieh@users.noreply.github.com>
2021-04-02 21:46:56 -0300

    Atual 02-04-2021

---
+++ Assets/Karting/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject lookMe;
+    public float velocity = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position,
+            target.transform.position, Time.smoothDeltaTime*velocity);
+        transform.LookAt(lookMe.transform);
+    }
+}
