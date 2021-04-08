546f13754e300018705ff0de0724df45db641eb4
Lucas Viana <lucasviana300@hotmail.com>
2021-04-08 01:20:51 -0300

    Mudanças Finais
    Adicionado a pista e a tela final

---
+++ Assets/KART/GoVitoria.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GoVitoria : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {   
+    
+    }
+     
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") )
+        {
+            SceneManager.LoadScene("WinPlayer1");
+        }
+        else if (other.CompareTag("Player2"))
+        {
+            
+            SceneManager.LoadScene("WinPlayer2");
+        }
+
+       
+
+    }
+}
